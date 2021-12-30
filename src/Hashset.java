adbaf09f84addf782637f48aaaf0c8b43b1442b0
vijay kaushal <vijaykaushal144@gmail.com>
2021-12-30 16:31:17 +0530

    introdution of Hashset

---
+++ src/Hashset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hashset {
+    public static void main(String[] args) {
+        HashSet h=new HashSet();
+        h.add("one");
+        h.add("Two");
+        h.add("Three");
+        h.add("Four");
+        System.out.println(h);
+    }
+}

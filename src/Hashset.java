f0076c5c3b11a7cf462ffeb3d07e4cf79a6e5259
vijay kaushal <vijaykaushal144@gmail.com>
2022-01-02 19:06:13 +0530

    introdution of hash set and linked hash set and custome hash set by help of student class

--- src/Hashset.java
+++ src/Hashset.java
@@ -1,11 +1,37 @@
 import java.util.*;
 public class Hashset {
     public static void main(String[] args) {
-        HashSet h=new HashSet();
-        h.add("one");
-        h.add("Two");
-        h.add("Three");
-        h.add("Four");
-        System.out.println(h);
+        //custome hash set;
+        Set<Student> st=new HashSet<>();
+        st.add(new Student("vijay",1));
+        st.add(new Student("Rafel",2));
+        st.add(new Student("shyam",3));
+        st.add(new Student("vijay",1));
+        System.out.println(st);
+//       Set<Integer> st=new HashSet<>();
+//       st.add(45);
+//       st.add(67);
+//       st.add(89);
+//       st.add(12);
+//       st.add(23);
+//       System.out.println(st);
+//        System.out.println(st.contains(24));
+//        System.out.println(st.isEmpty());
+//        System.out.println(st.size());
+//        st.clear();
+//        System.out.println(st);
+//        Set<Integer> st=new LinkedHashSet<>();
+//        st.add(45);
+//        st.add(67);
+//        st.add(89);
+//        st.add(12);
+//        st.add(23);
+//        System.out.println(st);
+//        System.out.println(st.contains(24));
+//        System.out.println(st.isEmpty());
+//        System.out.println(st.size());
+//        st.clear();
+//        System.out.println(st);
+
     }
 }
